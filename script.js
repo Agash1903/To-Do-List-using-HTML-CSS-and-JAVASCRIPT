1b52233917e401dc3acefe2dc7b71b6225c72bdd
Agash. A <141730950+Agash1903@users.noreply.github.com>
2024-08-15 13:52:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function add() {
+    var task = document.getElementById("newTask").value;
+    if (task) {
+      var list = document.getElementById("taskList");
+      var item = document.createElement("li");
+      item.appendChild(document.createTextNode(task));
+      var delButton = document.createElement("button");
+      delButton.textContent = "DELETE";
+      delButton.addEventListener("click", function() {
+        list.removeChild(item);
+      });
+      item.appendChild(delButton);
+      list.appendChild(item);
+      document.getElementById("newTask").value = "";
+    }
+  }
+  
